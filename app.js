08c12ab53fa3cf5064e2d2798275f42c3ae51cff
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-10-28 15:45:53 +0300

    purecss  added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const cool = require('cool-ascii-faces')
 const port = 3000
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port} ${cool()}`)
 })
