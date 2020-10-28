cd16340bcf59fd921f8335e90de2296ef6993234
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-10-28 18:38:20 +0300

    design implemented

---
+++ public/javascripts/index.js
@@ -0,0 +1,7 @@
+var intViewportHeight = window.innerHeight;
+
+document.getElementById("about").style.top = intViewportHeight + "px";
+document.getElementById("about").style.height = intViewportHeight + "px";
+
+document.getElementById("contacts").style.top = intViewportHeight * 2 + "px";
+document.getElementById("contacts").style.height = intViewportHeight + "px";
